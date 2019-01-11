81d192a6b4c7530d60cbddc6cf2b096322c33445
Anushan Lingam <5958017+AnushanLingam@users.noreply.github.com>
2019-01-11 20:26:53 +0000

    Add fade effect to login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {startLogin} from '../actions/auth';
+import Fade from 'react-reveal/Fade';
+import { connect } from 'react-redux';
+import { startLogin } from '../actions/auth';
 
-export const Login = ({startLogin}) => (
+export const Login = ({ startLogin }) => (
 
     <div className={"box-layout"}>
-        <div className={"box-layout__box"}>
-            <h1 className={"box-layout__title"}>Money Monitor</h1>
-            <p>Take control of your spending.</p>
-            <button className={"button"} onClick={startLogin}>Login with Google</button>
-        </div>
+        <Fade>
+            <div className={"box-layout__box"}>
+                <h1 className={"box-layout__title"}>Money Monitor</h1>
+                <p>Take control of your spending.</p>
+                <button className={"button"} onClick={startLogin}>Login with Google</button>
+            </div>
+        </Fade>
     </div>
 );
 
